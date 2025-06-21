78fbce49b876614e4f3fd5d59df68949fbbc7e76
saiKelkar <kelkars1999@gmail.com>
2025-06-21 13:42:11 +0530

    day-2 updated

---
+++ Day_2 - Data Types and Manipulate Strings/data_types.py
@@ -0,0 +1,21 @@
+# Strings
+
+# Subscripting
+print("Hello"[0]) # H
+print("Hello"[-1]) # o
+
+# Concatenate
+print("123" + "345") # 123345
+
+# Integer = Whole numbers
+print(123 + 345) # 468
+
+# Large Integers - underscores helps us humans to read them in a better way
+print(123_345_567) # 123345567
+
+# Float = Decimal
+print(3.14159)
+
+# Boolean = True or False
+print(True)
+print(False)
---
+++ Day_2 - Data Types and Manipulate Strings/exercise_4.py
@@ -0,0 +1,9 @@
+# BMI Calculator
+
+height = 1.65 
+weight = 84
+
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(bmi)
